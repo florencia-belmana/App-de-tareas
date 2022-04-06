60186c55e600dc1da8810747fcf635e36417c662
florencia-belmana <florbel_03@hotmail.com>
2022-04-06 19:48:47 -0300

    module exports / funcionalidad completa

--- app.js
+++ app.js
@@ -1,46 +1,37 @@
-let fs = require('fs') ;
-/* const { parse, dirname } = require('path');  *//* esto se pone solo */
-let path = require('path');
-let archivoTareas = path.join(__dirname, 'tareas.json');
+let tareas = require('./tareas');
 
-function leerTareas(){
-    let tareas = fs.readFileSync(archivoTareas, 'utf-8');
-    return JSON.parse(tareas);   
+let accion = process.argv[2]; // Primer parámetro ingresado en la terminal
+let parametros = process.argv.slice(3);
+
+const ESTADO_INICIAL = 'pendiente';
+
+switch (accion) {
+    case undefined:
+    case 'listar':
+    case 'l':
+        if (parametros.length > 0) {
+            tareas.listarPorEstado(parametros[0]);
+        } else {
+            tareas.listar();
+        };
+        break;
+    case 'crear':
+        if(parametros.length > 0) {
+            let tarea = {
+                titulo: parametros[0],
+                descripcion: parametros[1],
+                estado: ESTADO_INICIAL
+            }
+            tareas.crear(tarea);
+        } else {
+            console.log('Debes escribir al menos un título para la tarea');
+        }
+        break;
+    case 'detalle':
+        // 
+    default:
+        console.log('No entiendo ese comando');
 }
 
-function listarTareas(){
-    let tareas = leerTareas(); 
-    tareas.forEach(function(tarea){
-        console.log(tarea.titulo); 
-       });
-}
-
-function listarTareasPendientes(){
-    let tareas = leerTareas();
-
-    let listarTareasPendientes = tareas.filter(function(tarea){
-        return tarea.estado == 'pendiente';
-    });
-
-    listarTareasPendientes.forEach(function(tarea){
-         console.log(tarea.titulo); 
-       });
-
-}
-
-listarTareas();
-console.log();
-listarTareasPendientes();
-
-//CREAR
-/* let nuevaTarea = {
-    "titulo": "Estudiar Boostraps",
-    "descripcion": "",
-    "estado": "lista"
-   };
-
-tareas.push(nuevaTarea);
-console.log(tareas);
-
-let tareasJson = JSON.stringify(tareas, null, ' '); //solo para dar formato
-fs.writeFileSync(archivoTareas, tareasJson); */
+// tareas.listar();
+// tareas.listarPendientes();
