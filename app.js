642a329b1435cff4b06a1e6b81ede845242ab33f
florencia-belmana <florbel_03@hotmail.com>
2022-04-06 12:58:49 -0300

    leer,listar, pendientes

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 let fs = require('fs') ;
 /* const { parse, dirname } = require('path');  *//* esto se pone solo */
 let path = require('path');
-/* let archivoTareas = path.join(__dirname, 'tareas.json'); */
+let archivoTareas = path.join(__dirname, 'tareas.json');
 
+function leerTareas(){
+    let tareas = fs.readFileSync(archivoTareas, 'utf-8');
+    return JSON.parse(tareas);   
+}
 
-//LISTAR
-let tareas = fs.readFileSync(path.join(__dirname, 'tareas.json'), 'utf-8');
-    tareas = JSON.parse(tareas);   
+function listarTareas(){
+    let tareas = leerTareas(); 
+    tareas.forEach(function(tarea){
+        console.log(tarea.titulo); 
+       });
+}
 
- tareas.forEach(function(tarea){
- console.log(tarea.titulo); 
-});
+function listarTareasPendientes(){
+    let tareas = leerTareas();
+
+    let listarTareasPendientes = tareas.filter(function(tarea){
+        return tarea.estado == 'pendiente';
+    });
+
+    listarTareasPendientes.forEach(function(tarea){
+         console.log(tarea.titulo); 
+       });
+
+}
+
+listarTareas();
+console.log();
+listarTareasPendientes();
 
 //CREAR
-
-let nuevaTarea = {
-    "titulo": "Estudiar Boostraps 5",
+/* let nuevaTarea = {
+    "titulo": "Estudiar Boostraps",
     "descripcion": "",
     "estado": "lista"
-   }
+   };
+
+tareas.push(nuevaTarea);
+console.log(tareas);
+
+let tareasJson = JSON.stringify(tareas, null, ' '); //solo para dar formato
+fs.writeFileSync(archivoTareas, tareasJson); */
